a269f2fbf5f8a96cba902bd7aa6f0b02c29739f4
jaejeonglee <https://github.com/jaejeonglee>
2022-03-15 13:15:23 +0900

    :heavy_plus_sign: ADD route.js

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const port = 3000;
 const host = '0.0.0.0';
 
 const app = express();
+const productRoutes = require('./route');
+
+
+app.use(express.json())
+
+app.use('/api/products', productRoutes)
+
 app.get('/', (req,res) => {
     res.send('Hello World');
 });
