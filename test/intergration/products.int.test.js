9ca6bf2142f51809db628ab471a904fab8c18f78
jaejeonglee <https://github.com/jaejeonglee>
2022-03-18 04:39:35 +0900

    :zap: UPDATE  statusCode test for intergration test

--- test/intergration/products.int.test.js
+++ test/intergration/products.int.test.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const request = require('supertest')
 const app = require('../../server')
 const newProduct = require('../data/new-product.json')
@@ -9,4 +10,12 @@ it('POST /api/products', async () => {
     expect(response.statusCode).toBe(201)// 상태코드 확인
     expect(response.body.name).toBe(newProduct.name)// body의 name이 동일한지 확인
     expect(response.body.description).toBe(newProduct.description)// body의 description이 동일한지 확인
+})
+it('should return 500 on POST /api/products', async () => {
+    const response = await request(app)
+        .post('/api/products')
+        .send({name: "phone"})
+
+    expect(response.statusCode).toBe(500)// res의 statusCode가 500인지 확인
+   
 })
