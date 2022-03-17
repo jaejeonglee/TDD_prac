6c53c9c1653701d952bec3e577471d39cfd8aafa
jaejeonglee <https://github.com/jaejeonglee>
2022-03-17 16:32:26 +0900

    :white_check_mark: ADD Create메소드 unit test

--- controller/product.js
+++ controller/product.js
@@ -1,3 +1,6 @@
-exports.createProduct = () => {
-  
-}
+const productModel = require('../models/product')
+
+exports.createProduct = (req, res, next) => {
+  const createProduct = productModel.create(req.body)
+  return res.status(201).json(createProduct)
+} 
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,7 +1,46 @@
 const productController = require('../../controller/product')
+const productModel =require('../../models/product')
+const httpMocks = require('node-mocks-http')//?
+const newProduct = require('../data/new-product.json')
 
-describe('product controller create', () => {
+//Mock 함수 생성
+productModel.create = jest.fn()
+
+//저장할 데이터 인위적으로 만들기
+//beforeEach를 이용하면 공통된 코드의 반복을 줄일 수 있음
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest()
+  res = httpMocks.createResponse()
+  next = null
+})
+
+describe('Product Controller Create', () => {
+
+  beforeEach(() => {
+    req.body = newProduct//미리 작성해둔 body 사용
+  })
+  
+  //createProduct함수가 함수로 동작하는게 맞는지 확인
   it('should have a createProduct function', () => {
     expect(typeof productController.createProduct).toBe('function')
-  })  
+  })
+  //createProduct함수를 호출할 때 Product Model의 Create 메소드가 호출되는지 확인
+  it('should call productModel.create', () => {
+      productController.createProduct(req, res, next)
+      expect(productModel.create).toBeCalledWith(newProduct)
+  })
+  //성공적으로 데이터를 create했을 시 상태코드를 201로 response보내는지 확인 
+  it('should return 201 response code', () => {
+    productController.createProduct(req, res, next)
+    expect(res.statusCode).toBe(201)
+    // expect(res._isEndCalled()).toBeTruthy()// res.send를 실제로 하고있다면 true이기 때문에 passed
+    expect(res._isJSON()).toBeTruthy()// res.json을 실제로 하고있다면 true이기 때문에 passed
+  })
+  //
+  it('should return json body in response', () => {
+    productModel.create.mockReturnValue(newProduct)//Mock함수로 create했을 때 return값은 newProduct
+    productController.createProduct(req, res, next)
+    expect(res._getJSONData()).toStrictEqual(newProduct)//response되는 json데이터가 newProduct랑 같은지 확인
+  })
 })
